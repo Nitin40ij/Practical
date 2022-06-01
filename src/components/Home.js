505b2425fe2404a0bf4a28cba9e1b62ed1ac09c0
Nitin Srivastav <nitin.srivastav_cs19@gla.ac.in>
2022-06-01 15:35:09 +0530

    First commit

---
+++ src/components/Home.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div style={{border:"double" , padding:"30px" ,margin:"10px 80px"}} className='container'>
+        <h1 className='heading1'>MINI PROJECT</h1>
+         <h2 className='heading2'>(2021-2022)</h2>
+         <h2 className='heading3'>Skill-Media</h2>
+         <h3 className='heading4'> Project Report</h3>
+        <img style={{height:"250px"}} src="/images/IMG-20220601-WA0001.jpg" alt="Not found" />
+         <h3 className='subheading'>Institute of Engineering and Technology</h3>
+         <h3> Submitted by - </h3>
+         <h3> Nitin Srivastav (191500517)</h3>
+         <h3> Poorvi Tiwari (191500549)</h3>
+         <h3> Nikunj Gupta (191500507)</h3>
+         <h3> Shaktiman Singh (191500400)</h3>
+         <h3>Under the supervision of </h3>
+         <h3> Mr. Piyush Vashisth</h3>
+         <h2>Asst Professor</h2>
+         <h4>Department of Computer Science and Application</h4>
+
+
+    </div>
+  )
+}
+
+export default Home
